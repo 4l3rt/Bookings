3092906d4067f69642f60514cd0b7ece02092b0e
sh1zz0 <github.gong542@passinbox.com>
2025-05-22 14:25:22 +0400

    finished basic booking frontend

--- Client/src/containers/header/Header.tsx
+++ Client/src/containers/header/Header.tsx
@@ -1,17 +1,20 @@
 import "./header.css"
-import  { useState, useMemo } from 'react';
+import  { useState, useMemo, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import ReactFlagsSelect from 'react-flags-select';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
+import { countryCodes } from "./countryData";
 
+const today = new Date();
 type RoomType = 'twin' | 'double' | 'family';
 const ROOM_RATES: Record<RoomType, number> = {
   twin: 90,
   double: 110,
   family: 130,
 };
+
 
 export  function Header() {
 
@@ -20,9 +23,75 @@ export  function Header() {
   const [checkOut, setCheckOut] = useState<Date | null>(null);
   const [adults, setAdults] = useState<number>(1);
   const [kids, setKids] = useState<number>(0);
+  const [name, setName] = useState<string>("")
   const [country, setCountry] = useState('US');
   const [phone, setPhone] = useState('');
 
+  const [nameError, setNameError] = useState<string>('');
+  const [phoneError, setPhoneError] = useState<string>('');
+  const [canSubmit, setCanSubmit] = useState<boolean>(false);
+  const [calendarError, setCalendarError] = useState<string | null>(null)
+
+  const addDays = (date: Date, days = 1) => {
+    const d = new Date(date);
+    d.setDate(d.getDate() + days);
+    return d;
+  };
+
+  const minCheckOut = useMemo(() => {
+    const base = checkIn || new Date();
+    return addDays(base, 1);
+  }, [checkIn]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Clear old error
+    setCalendarError(null);
+
+  }
+
+    
+
+  useEffect(() => {
+    if (name.length === 0) {
+      setNameError(''); // no feedback yet
+    } else if (name.length <= 2) {
+      setNameError('Name too short');
+    } else if (/[^a-zA-Z\s'-]/.test(name)) {
+      setNameError('No symbols allowed in name');
+    } else {
+      setNameError('');
+    }
+  }, [name]);
+
+  useEffect(() => {
+    const digits = phone.replace(/\D/g, '');
+    if (digits.length === 0) {
+      setPhoneError('');
+    } else if (digits.length < 5) {
+      setPhoneError('Enter a valid phone number');
+    } else {
+      setPhoneError('');
+    }
+  }, [phone]);
+
+useEffect(() => {
+    setCanSubmit(
+      nameError === '' &&
+      phoneError === '' &&
+      checkIn != null &&
+      checkOut != null &&
+      name.length > 2 &&
+      phone.replace(/\D/g, '').length >= 4
+    );
+  }, [
+    nameError,
+    phoneError,
+    checkIn,
+    checkOut,
+    name,
+    phone,
+  ]);
 
 const nights = useMemo(() => {
     if (!checkIn || !checkOut) return 0;
@@ -37,116 +106,164 @@ const nights = useMemo(() => {
   const roomRate = ROOM_RATES[room];
   const total = nights * roomRate;
 
+  
+
 
   return (
     <div className="sno__header" >
-        <div className="sno__header-booking-form color-transparent">
+        <form className="sno__header-booking-form color-transparent">
             <div className="sno__header-booking-form-input-spacer">
-                <div className="sno__header-booking-form-input-container input-medium ">
+
+                <label className="sno__header-booking-form-input-container  container-medium ">
+                  <p>Room</p>
                   <select
-          value={room}
-          onChange={e => setRoom(e.target.value as RoomType)}
-        >
-          <option value="twin">Twin Room (90GEL/night)</option>
-          <option value="double">Double Room (110GEL/night)</option>
-          <option value="family">Family Room (130GEL/night)</option>
-        </select>
-                </div>
-                <div className="sno__header-booking-form-input-container input-large "><p>Check In</p><DatePicker
-      className="input-picker"
-      selected={checkIn}
-      onChange={date => setCheckIn(date)}
-      selectsStart
-      startDate={checkIn}
-      endDate={checkOut}
-      placeholderText="MM/DD/YY"
-      onKeyDown={e => e.preventDefault()}
-      /></div>
-                <div className="sno__header-booking-form-input-container input-large "><p>Check Out</p><DatePicker
-      className="input-picker"
-      selected={checkOut}
-      onChange={date => setCheckOut(date)}
-      selectsStart
-      startDate={checkIn}
-      endDate={checkOut}
-      placeholderText="MM/DD/YY2"
-      onKeyDown={e => e.preventDefault()}
-    /></div>
-                <div className="sno__header-booking-form-input-container input-small " ><p>Adults</p><input
-                className="input-picker"
-                type="number"
-                min={1}
-                max={10}
-                value={adults}
-                onChange={e => setAdults(Number(e.target.value))}
-                onKeyDown={e => e.preventDefault()}
-                ></input></div>
-                <div className="sno__header-booking-form-input-container input-small "><p>Kids</p><input
-                className="input-picker"
-                type="number"
-                min={0}
-                max={10}
-                value={kids}
-                onChange={e => setKids(Number(e.target.value))}
-                onKeyDown={e => e.preventDefault()}
-
-
-                ></input></div>
-                <div className="sno__header-booking-form-input-container input-large"><p>Full Name</p><input></input></div>
-                <div className="sno__header-booking-form-input-container input-medium"><p>Country</p>
-                <ReactFlagsSelect
-                  countries={['AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AR', 'AS', 'AT', 'AU', 'AW', 'AX', 'AZ',
-                    'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS', 'BT', 'BV', 'BW', 'BY', 'BZ',
-                    'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CW', 'CX', 'CY', 'CZ',
-                    'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ',
-                    'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET',
-                    'FI', 'FJ', 'FM', 'FO', 'FR',
-                    'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GT', 'GU', 'GW', 'GY',
-                    'HK', 'HM', 'HN', 'HR', 'HT', 'HU',
-                    'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS', 'IT',
-                    'JE', 'JM', 'JO', 'JP',
-                    'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW', 'KY', 'KZ',
-                    'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY',
-                    'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ',
-                    'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ',
-                    'OM',
-                    'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PT', 'PW', 'PY',
-                    'QA',
-                    'RE', 'RO', 'RS', 'RU', 'RW',
-                    'SA', 'SB', 'SC', 'SD', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SX', 'SY', 'SZ',
-                    'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TZ',
-                    'UA', 'UG', 'UM', 'US', 'UY', 'UZ',
-                    'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU',
-                    'WF', 'WS',
-                    'YE', 'YT',
-                    'ZA', 'ZM', 'ZW']}
-                  selected={country}
-                  onSelect={code => setCountry(code)}
-                  showSelectedLabel={false}
-                  showOptionLabel={false}
-                  className="flag-btn"
-                  searchable={true}
-                /></div>
-                <div className="sno__header-booking-form-input-container input-large"><p className="phone-costum-margin">Phone Number</p><PhoneInput
-                country={country.toLowerCase()}
-                value={phone}
-                onChange={setPhone}
-                buttonStyle={{ display: 'none' }}
-                inputClass="custom-phone-input"
-              /></div>
-                <div className="sno__header-booking-form-input-container input-mdeium"><p>How should we notify you?</p>
-                  <select
-
+                  className="sno__header-input"
+                  value={room}
+                  onChange={e => setRoom(e.target.value as RoomType)}
                   >
+                    <option value="twin">Twin Room (90GEL/night)</option>
+                    <option value="double">Double Room (110GEL/night)</option>
+                    <option value="family">Family Room (130GEL/night)</option>
+                  </select>
+                </label>
+
+                <label className="sno__header-booking-form-input-container  container-large ">
+                  <p>Check In</p>
+                  <DatePicker
+                    portalId="root-portal"
+                    className="sno__header-input input-picker"
+                    selected={checkIn}
+                    minDate={today}
+                    onChange={date => setCheckIn(date)}
+                    selectsStart
+                    startDate={checkIn}
+                    endDate={checkOut}
+                    placeholderText="MM/DD/YY"
+                    onKeyDown={e => e.preventDefault()}
+                    shouldCloseOnSelect={true}
+                  />
+                  {calendarError && <p className="error">{calendarError}</p>}
+                </label>
+
+                <label className="sno__header-booking-form-input-container container-large ">
+                  <p>Check Out</p>
+                  <DatePicker
+                    portalId="root-portal"
+                    className="sno__header-input input-picker"
+                    selected={checkOut}
+                    onChange={date => setCheckOut(date)}
+                    selectsStart
+                    startDate={checkIn}
+                    minDate={minCheckOut}
+                    endDate={checkOut}
+                    placeholderText="MM/DD/YY"
+                    onKeyDown={e => e.preventDefault()}
+                    shouldCloseOnSelect={true}
+                    />
+                </label>
+
+                <label className="sno__header-booking-form-input-container container-small " >
+                  <p>Adults</p>
+                  <input
+                    className="sno__header-input input-picker"
+                    type="number"
+                    min={1}
+                    max={6}
+                    value={adults}
+                    onChange={e => setAdults(Number(e.target.value))}
+                    onKeyDown={e => e.preventDefault()}
+                    >
+                  </input>
+                </label>
+
+                <label className="sno__header-booking-form-input-container container-small ">
+                  <p>Kids</p>
+                  <input
+                    className="sno__header-input input-picker"
+                    type="number"
+                    min={0}
+                    max={5}
+                    value={kids}
+                    onChange={e => setKids(Number(e.target.value))}
+                    onKeyDown={e => e.preventDefault()} 
+                    >
+                    </input>
+                </label>
+
+                <label className="sno__header-booking-form-input-container container-large">
+                  <p>Full Name</p>
+                  <input 
+                    className="sno__header-input"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    placeholder="Your full name"
+                    >
+                  </input>
+                  {nameError && <p className="error">{nameError}</p>}
+                </label>
+
+                <label  className="sno__header-booking-form-input-container container-medium" >
+                  <p>Country</p>
+                  <ReactFlagsSelect
+                    countries={countryCodes}
+                    selected={country}
+                    onSelect={code => setCountry(code)}
+                    showSelectedLabel={false}
+                    showOptionLabel={false}
+                    className="flag-btn"
+                    searchable={true}
+
+
+                  />
+                </label>
+
+                <label className="sno__header-booking-form-input-container container-large">
+                  <p className="phone-costum-margin">Phone Number</p>
+                  <PhoneInput
+                  country={country.toLowerCase()}
+                  value={phone}
+                  onChange={setPhone}
+                  buttonStyle={{ display: 'none' }}
+                  inputClass="custom-phone-input"
+                  />
+                  {phoneError && <div className="error">{phoneError}</div>}
+                </label>
+
+                <div className="sno__header-booking-form-input-container container-mdeium">
+
+                {checkIn && checkOut && (
+                <p className="sno__total" ><strong>Total: {total.toFixed(2)} GEL </strong></p>
+                )}
+
+                  <p className="notify-costum-margin">How should we notify you?</p>
+                  <select>
                     <option value="twin">Whatsapp</option>
                     <option value="double">Signal</option>
+                    <option value="double">Telegram</option>
                     <option value="family">SMS</option>
-                    </select>
-                </div>
-                <p><strong>Total: {total.toFixed(2)} GEL </strong></p>
-            </div>
-            <p>Problem booking? no worries! Contact us</p>
-        </div>
+                  </select>
+
+                  { canSubmit && (
+                    <p className="sno__header-form-keepInMind" ><b>Note:</b> Please make sure the entered phone number is correct. If you don't reply to our confirmation in a day or two we will have to cancel your booking ;(( </p>
+
+                  )}
+
+
+                  <button
+                      className="son__booking-submit-btn"
+                      type="submit"
+                      disabled={!canSubmit}
+                      style={{
+                        opacity: canSubmit ? 1 : 0.5,
+                        cursor: canSubmit ? 'pointer' : 'not-allowed'
+                      }}
+                      onClick={handleSubmit}
+                  >Book Now</button>
+
+                 </div>
+              </div>
+            <p className="sno__header-form-feepInMind">Problem booking or any qestions?  <p>no worries! just <a href="2">let us know</a></p></p>
+        </form>
     </div>
   )
 }
